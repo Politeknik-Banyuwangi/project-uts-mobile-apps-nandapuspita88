3171745209ec3f70e923b5ceb4687942afba6e7f
FlutLab.io <>
2022-05-31 03:57:00 +0000

    Autosave

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -103,7 +103,7 @@ class _CameraState extends State<Camera> {
     return const Scaffold(
       body: Center(
         child: Text(
-          "Your Name + Nim",
+          "Nanda Ayu Puspita (362055401118)",
           style: TextStyle(color: Colors.teal, fontWeight: FontWeight.bold, fontSize: 30),
         ),
       ),
